700e718c82fdeef0bff53d6053e905fa449e0183
Boiunets Denys <boyunets.dv@gmail.com>
2020-11-17 18:16:03 +0200

    second stage completed

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,11 +1,25 @@
 package machine
 
+import java.util.*
+
 fun main() {
-    println("Starting to make a coffee\n" +
-            "Grinding coffee beans\n" +
-            "Boiling water\n" +
-            "Mixing boiled water with crushed coffee beans\n" +
-            "Pouring coffee into the cup\n" +
-            "Pouring some milk into the cup\n" +
-            "Coffee is ready!")
+    println("Write how many cups of coffee you will need:")
+    val scanner = Scanner(System.`in`)
+    val cupsCount = scanner.nextInt()
+    CoffeMachine().calculateResources(cupsCount)
+}
+
+class CoffeMachine {
+    companion object {
+        private const val WATER_PER_CUP = 200
+        private const val MILK_PER_CUP = 50
+        private const val BEANS_PER_CUP = 15
+    }
+
+    fun calculateResources(countOfCups: Int) {
+        println("For $countOfCups of coffee you will need:")
+        println("${countOfCups * WATER_PER_CUP} ml of water")
+        println("${countOfCups * MILK_PER_CUP} ml of milk")
+        println("${countOfCups * BEANS_PER_CUP} g of coffee beans")
+    }
 }
