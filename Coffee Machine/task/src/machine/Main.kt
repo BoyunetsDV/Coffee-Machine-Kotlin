011c542e43e2b5460c1671dc16a0c5d73b30db94
Boiunets Denys <boyunets.dv@gmail.com>
2020-11-18 00:54:45 +0200

    stage 6 completed

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -3,7 +3,18 @@ package machine
 import java.util.*
 
 fun main() {
-    CoffeeShop().beginWork()
+    val scanner = Scanner(System.`in`)
+
+    val coffeeMachine = CoffeeMachine()
+    coffeeMachine.displayInitialMessage()
+
+    while (true) {
+        val command = scanner.next()
+        if (command == "exit") {
+            break
+        }
+        coffeeMachine.makeAction(command)
+    }
 }
 
 enum class CoffeeType(val waterPerCup: Int, val milkPerCup: Int, val beansPerCup: Int, val price: Int) {
@@ -12,77 +23,95 @@ enum class CoffeeType(val waterPerCup: Int, val milkPerCup: Int, val beansPerCup
     CAPPUCCINO(200, 100, 12, 6)
 }
 
-class CoffeeShop {
-    private val scanner = Scanner(System.`in`)
-    private val coffeeMachine = CoffeeMachine()
-
-    fun beginWork() {
-        configureCoffeeMachine()
-        while (true) {
-            print("Write action (buy, fill, take, remaining, exit): ")
-            val action = scanner.next()!!
-            if (action == "exit") {
-                break
-            }
-            makeAction(action)
-        }
-    }
-
-    private fun configureCoffeeMachine() {
-        coffeeMachine.setResources(400, 540, 120, 550, 9)
-    }
-
-    private fun printCoffeeMachineState() {
-        coffeeMachine.printState()
-    }
-
-    private fun makeAction(action: String) {
-        println()
-        when (action) {
-            "buy" -> buyACupOfCoffee()
-            "fill" -> fillCoffeeMachineWithResources()
-            "take" -> takeCashFromCoffeeMachine()
-            "remaining" -> printCoffeeMachineState()
-        }
-        println()
-    }
-
-    private fun buyACupOfCoffee() {
-        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-        when (scanner.next()) {
-            "1" -> coffeeMachine.buy(CoffeeType.ESPRESSO)
-            "2" -> coffeeMachine.buy(CoffeeType.LATTE)
-            "3" -> coffeeMachine.buy(CoffeeType.CAPPUCCINO)
-            "back" -> return
-        }
-    }
-
-    private fun fillCoffeeMachineWithResources() {
-        print("Write how many ml of water do you want to add: ")
-        val water = scanner.nextInt()
-        print("Write how many ml of milk do you want to add: ")
-        val milk = scanner.nextInt()
-        print("Write how many grams of coffee beans do you want to add: ")
-        val beans = scanner.nextInt()
-        print("Write how many disposable cups of coffee do you want to add: ")
-        val cups = scanner.nextInt()
-
-        coffeeMachine.addResources(water, milk, beans, cups)
-    }
-
-    private fun takeCashFromCoffeeMachine() {
-        println("I gave you \$${coffeeMachine.getCash()}")
-    }
+enum class State {
+    MAIN_MENU, BUY, FILL_WATER, FILL_MILK, FILL_COFFEE_BEANS, FILL_CUPS
 }
 
-class CoffeeMachine() {
-    private var waterCount: Int = 0
-    private var milkCount: Int = 0
-    private var beansCount: Int = 0
-    private var cash: Int = 0
-    private var disposableCups: Int = 0
 
-    fun setResources(_waterCount: Int, _milkCount: Int, _beansCount: Int, _cash: Int, _disposableCups: Int) {
+class CoffeeMachine {
+    private var state: State = State.MAIN_MENU
+    private var waterCount: Int = 400
+    private var milkCount: Int = 540
+    private var beansCount: Int = 120
+    private var disposableCups: Int = 9
+    private var cash: Int = 550
+
+    fun displayInitialMessage() {
+        print("Write action (buy, fill, take, remaining, exit): ")
+    }
+
+    fun makeAction(command: String) {
+        when {
+            state == State.MAIN_MENU && command == "buy" -> {
+                println()
+                print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+                state = State.BUY
+            }
+            state == State.MAIN_MENU && command == "remaining" -> {
+                println()
+                printState()
+                println()
+                displayInitialMessage()
+            }
+            state == State.MAIN_MENU && command == "take" -> {
+                println()
+                println("I gave you \$${getCash()}")
+                println()
+                displayInitialMessage()
+            }
+            state == State.MAIN_MENU && command == "fill" -> {
+                println()
+                print("Write how many ml of water do you want to add: ")
+                state = State.FILL_WATER
+            }
+            state == State.FILL_WATER -> {
+                addResource(command.toInt())
+                print("Write how many ml of milk do you want to add: ")
+                state = State.FILL_MILK
+            }
+            state == State.FILL_MILK -> {
+                addResource(command.toInt())
+                print("Write how many grams of coffee beans do you want to add: ")
+                state = State.FILL_COFFEE_BEANS
+            }
+            state == State.FILL_COFFEE_BEANS -> {
+                addResource(command.toInt())
+                print("Write how many disposable cups of coffee do you want to add: ")
+                state = State.FILL_CUPS
+            }
+            state == State.FILL_CUPS -> {
+                addResource(command.toInt())
+                println()
+                displayInitialMessage()
+                state = State.MAIN_MENU
+            }
+            state == State.BUY && command == "back" -> {
+                println()
+                displayInitialMessage()
+                state = State.MAIN_MENU
+            }
+            state == State.BUY && command == "1" -> {
+                buy(CoffeeType.ESPRESSO)
+                println()
+                displayInitialMessage()
+                state = State.MAIN_MENU
+            }
+            state == State.BUY && command == "2" -> {
+                buy(CoffeeType.LATTE)
+                println()
+                displayInitialMessage()
+                state = State.MAIN_MENU
+            }
+            state == State.BUY && command == "3" -> {
+                buy(CoffeeType.CAPPUCCINO)
+                println()
+                displayInitialMessage()
+                state = State.MAIN_MENU
+            }
+        }
+    }
+
+    private fun setResources(_waterCount: Int, _milkCount: Int, _beansCount: Int, _cash: Int, _disposableCups: Int) {
         waterCount = _waterCount
         milkCount = _milkCount
         beansCount = _beansCount
@@ -90,30 +119,39 @@ class CoffeeMachine() {
         disposableCups = _disposableCups
     }
 
-    fun addResources(_waterCount: Int, _milkCount: Int, _beansCount: Int, _disposableCups: Int) {
+    private fun addResource(resourceCount: Int) {
+        when (state) {
+            State.FILL_WATER -> waterCount += resourceCount
+            State.FILL_MILK -> milkCount += resourceCount
+            State.FILL_COFFEE_BEANS -> beansCount += resourceCount
+            State.FILL_CUPS -> disposableCups += resourceCount
+        }
+    }
+
+    private fun addResources(_waterCount: Int, _milkCount: Int, _beansCount: Int, _disposableCups: Int) {
         waterCount += _waterCount
         milkCount += _milkCount
         beansCount += _beansCount
         disposableCups += _disposableCups
     }
 
-    fun calcResourcesNeededForCups(countOfCups: Int, coffeeType: CoffeeType) {
+    private fun calcResourcesNeededForCups(countOfCups: Int, coffeeType: CoffeeType) {
         println("For $countOfCups of coffee you will need:")
         println("${countOfCups * coffeeType.waterPerCup} ml of water")
         println("${countOfCups * coffeeType.milkPerCup} ml of milk")
         println("${countOfCups * coffeeType.beansPerCup} g of coffee beans")
     }
 
-    fun printState() {
+    private fun printState() {
         println("The coffee machine has:")
         println("$waterCount of water")
         println("$milkCount of milk")
         println("$beansCount of coffee beans")
         println("$disposableCups of disposable cups")
-        println("$cash of money")
+        println("\$$cash of money")
     }
 
-    fun checkIfAvailableResourcesForNumberOfCups(numberOfCups: Int, coffeeType: CoffeeType) {
+    private fun checkIfAvailableResourcesForNumberOfCups(numberOfCups: Int, coffeeType: CoffeeType) {
         val maxNumberOfCups = getMaximumNumberOfCups(coffeeType)
         when {
             maxNumberOfCups == numberOfCups -> {
@@ -128,7 +166,7 @@ class CoffeeMachine() {
         }
     }
 
-    fun getMaximumNumberOfCups(coffeeType: CoffeeType): Int {
+    private fun getMaximumNumberOfCups(coffeeType: CoffeeType): Int {
         val waterPortions = if (coffeeType.waterPerCup == 0) Int.MAX_VALUE else waterCount / coffeeType.waterPerCup
         val milkPortions = if (coffeeType.milkPerCup == 0) Int.MAX_VALUE else milkCount / coffeeType.milkPerCup
         val beansPortions = if (coffeeType.beansPerCup == 0) Int.MAX_VALUE else beansCount / coffeeType.beansPerCup
@@ -136,7 +174,7 @@ class CoffeeMachine() {
         return listOf(waterPortions, milkPortions, beansPortions, disposableCups).minOrNull() ?: 0
     }
 
-    fun buy(coffeeType: CoffeeType) {
+    private fun buy(coffeeType: CoffeeType) {
         when {
             waterCount < coffeeType.waterPerCup -> println("Sorry, not enough water!")
             milkCount < coffeeType.milkPerCup -> println("Sorry, not enough milk!")
@@ -153,7 +191,7 @@ class CoffeeMachine() {
         }
     }
 
-    fun getCash(): Int {
+    private fun getCash(): Int {
         val result = cash
         cash = 0
         return result
